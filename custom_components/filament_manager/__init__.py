6582fc61f378288a40c885ebff1d709c4556a9eb
Bryan Thoury <bryan.thoury@live.fr>
2024-09-17 20:59:16 +0200

    1.0.9

--- custom_components/filament_manager/__init__.py
+++ custom_components/filament_manager/__init__.py
@@ -1,2 +1,19 @@
-async def async_setup(hass, config):
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up Filament Manager from YAML configuration."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Filament Manager from a config entry."""
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+    # Ici, tu pourrais initialiser d'autres composants comme des capteurs ou des services liés à ton plugin.
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle removal of a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
     return True
