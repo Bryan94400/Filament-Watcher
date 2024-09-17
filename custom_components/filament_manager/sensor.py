a942d5d6b17b4c0dcb20166686f0625115eb98e0
Bryan Thoury <bryan.thoury@live.fr>
2024-09-17 19:41:01 +0200

    sensorpi

---
+++ custom_components/filament_manager/sensor.py
@@ -0,0 +1,33 @@
+from homeassistant.helpers.entity import Entity
+from homeassistant import config_entries
+from homeassistant.const import CONF_NAME
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the filament sensors."""
+    sensors = []
+    # Exemple de capteur pour le stock de filament
+    sensors.append(FilamentSensor("Filament Color", "blue", 5))  # Exemple de couleur et quantité
+    async_add_entities(sensors)
+
+class FilamentSensor(Entity):
+    """Representation of a filament sensor."""
+
+    def __init__(self, name, color, quantity):
+        self._name = name
+        self._color = color
+        self._quantity = quantity
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._name} ({self._color})"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._quantity
+
+    @property
+    def icon(self):
+        """Return the icon for the sensor."""
+        return "mdi:printer-3d"  # Choisis une icône appropriée
